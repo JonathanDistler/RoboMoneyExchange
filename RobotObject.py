740f0d704986a9abf41ba6272e69db6bd7390228
Jonathan Distler <jhd94@cornell.edu>
2025-08-10 12:18:09 -0400

    updates comments, adds a trade

--- RobotObject.py
+++ RobotObject.py
@@ -1,57 +1,59 @@
-#could create an object class with each having a special code that the other needs to verify to pass the crypto cost
-#could also figure out how to link "bank accounts" to help the transfer between the robots 
-#next step is to add functionality to make sure "transactions" are secure-could look into different cybersecurity protocols
 class RecieverRobot:
+    #defines functions for accessing robot name, account amount, as well as paying for service 
+    #could also add logs of which services were provided and how much they cost, as well as different service types
     def __init__(self, name, account, reciever_id):
-        self.name=name
-        self.account_amount=account
-        #private variable to help in future transactions, to make sure sender and reciever are accurate and no bad "actors" 
-        self.__private_id=reciever_id
-    #removes money from account based on cost of product
-    def account_dec(self,decrease):
-        self.account_amount-=decrease
-    #returns name
-    def robot_name (self):
-        return(self.name)
-    #returns account amount
+        self._name = name
+        self._account_amount = account
+        self.__private_id = reciever_id
+
+    def account_dec(self, decrease):
+        self._account_amount -= decrease
+
+    def robot_name(self):
+        return self._name
+
     def account_amount(self):
-        return(self.account_amount)
+        return self._account_amount
+
 
 class SenderRobot:
-    def __init__(self, name, service, service_cost, account,sender_id):
-        self.name=name
-        self.service=service
-        self.service_cost=service_cost
-        self.account_amount=account
-        self.__private_id=sender_id
+    #defines functions fo raccessing robot name, service, and account amount
+    #could change the function so the other has to have more than enough money
+    #might need a check function to make sure the other is the correct one, too
+    def __init__(self, name, service, service_cost, account, sender_id):
+        self._name = name
+        self.service = service
+        self._service_cost = service_cost
+        self._account_amount = account
+        self.__private_id = sender_id
 
-    #adds money to account based on how much product costs
-    def account_add(self,increase):
-        self.account_amount+=increase
-    #returns name
+    def account_add(self, increase):
+        self._account_amount += increase
+
     def robot_name(self):
-        return(self.name)
-    #returns the service of the robot, ie "cleaning"
+        return self._name
+
     def robot_service(self):
-        return(self.service)
-    #returns the cost of the service, like an appraisal
+        return self.service
+
     def service_cost(self):
-        return(self.service_cost)
-    #returns the account amount or balance of the sender
+        return self._service_cost
+
     def account_amount(self):
-        return(self.account_amount)
-    
-#name, service action, the cost of the service, account amount, and id number (for future work verifiying transactions)
+        return self._account_amount
 
-ex_sender_obj=SenderRobot("SoFi","Car Cleaning",250,0,21929)
-ex_reciever_obj=RecieverRobot("SoFi2",500,43348)
+    def trade(self, robotObj, name):
+        if name == robotObj.robot_name():
+            cost = self.service_cost()
+            self.account_add(cost)
+            robotObj.account_dec(cost)
 
-#example of setup
-service_cost=ex_sender_obj.service_cost
-ex_sender_obj.account_add(service_cost)
-sender_amount=ex_sender_obj.account_amount
-print("Sender amount:",sender_amount)
 
-ex_reciever_obj.account_dec(service_cost)
-reciever_amount=ex_reciever_obj.account_amount
-print("Reciever", reciever_amount)
+# Example of usage
+ex_sender_obj = SenderRobot("SoFi", "Car Cleaning", 250, 0, 21929)
+ex_reciever_obj = RecieverRobot("SoFi2", 500, 43348)
+
+ex_sender_obj.trade(ex_reciever_obj, "SoFi2")
+
+print("Receiver amount:", ex_reciever_obj.account_amount())
+print("Sender amount:", ex_sender_obj.account_amount())

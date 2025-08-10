e9920382c1b460ec4544b79a752add97b08f446e
Jonathan Distler <jhd94@cornell.edu>
2025-08-10 10:20:05 -0400

    creates a script to show off robot class

--- RobotObject.py
+++ RobotObject.py
@@ -1,4 +1,5 @@
 #could create an object class with each having a special code that the other needs to verify to pass the crypto cost
+#could also figure out how to link "bank accounts" to help the transfer between the robots 
 class RecieverRobot:
     def __init__(self, name, account, reciever_id):
         self.name=name
@@ -14,7 +15,7 @@ class RecieverRobot:
     def account_amount(self):
         return(self.account_amount)
 
-class senderRobot:
+class SenderRobot:
     def __init__(self, name, service, service_cost, account,sender_id):
         self.name=name
         self.service=service
@@ -34,4 +35,19 @@ class senderRobot:
     def service_cost(self):
         return(self.service_cost)
     
+    def account_amount(self):
+        return(self.account_amount)
     
+#name, service action, the cost of the service, account amount, and id number (for future work verifiying transactions)
+
+ex_sender_obj=SenderRobot("SoFi","Car Cleaning",250,0,21929)
+ex_reciever_obj=RecieverRobot("SoFi2",500,43348)
+
+service_cost=ex_sender_obj.service_cost
+ex_sender_obj.account_add(service_cost)
+sender_amount=ex_sender_obj.account_amount
+print("Sender amount:",sender_amount)
+
+ex_reciever_obj.account_dec(service_cost)
+reciever_amount=ex_reciever_obj.account_amount
+print("Reciever", reciever_amount)
